3bc84aa07b8370c8a3f733ccd977d4b2e326898a
ADITI SINGH <aditiisinghh04@gmail.com>
2025-10-20 23:16:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Count Digits/count-digits.java
+++ Difficulty: Easy/Count Digits/count-digits.java
@@ -1,15 +1,15 @@
 class Solution {
     public int countDigits(int n) {
-        int count=0;
+        // code here
         int rem=0;
-        int x=n;
-        while(x>0)
+        int num=n;
+        int count=0;
+        while(num>0)
         {
-            rem=x%10;
+            rem=num%10;
             count++;
-            x=x/10;
+            num=num/10;
         }
         return count;
-        
     }
 }
